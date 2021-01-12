32b9d5f81204b5f4ac9fb145e0edc4e245d3dc20
Daniel Sim <me@danielsim.co.uk>
2021-01-12 12:27:29 +0100

    ...whoops, commit the routes I missed from last push

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 require "resque_web"
 
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-
-  post "/graphql", to: "graphql#execute"
   mount ResqueWeb::Engine => "/resque_web"
 
   devise_for :users
