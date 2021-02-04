f0e34165d77ba1aa9b8fdc4051ab76fbb1fd7668
FORSBERG+two <bjorn@forsbergplustwo.com>
2021-02-04 08:27:43 +0100

    Reduce historical data to 3 years

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -5,7 +5,7 @@ class ImportWorker
     return unless current_user_id
     current_user = User.find(current_user_id)
     last_calculated_metric = current_user.metrics.maximum("metric_date")
-    last_calculated_metric_date = last_calculated_metric.presence || 60.months.ago.to_date # question for Bjorn: any reason for 5 years?
+    last_calculated_metric_date = last_calculated_metric.presence || 36.months.ago.to_date
     if !filename.nil?
       Rails.logger.info("CSV IMPORT")
       PaymentHistory.import_csv(current_user, last_calculated_metric_date, filename)
