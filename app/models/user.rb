79ec3e78d0ecd78e4937d6a19be2d26cb59be5c2
FORSBERG+two <bjorn@forsbergplustwo.com>
2021-09-15 09:10:40 +0200

    Use delete_all for dependants destruction when removing account

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :payment_histories, dependent: :destroy
-  has_many :metrics, dependent: :destroy
+  has_many :payment_histories, dependent: :delete_all
+  has_many :metrics, dependent: :delete_all
 
   def has_partner_api_credentials?
     partner_api_access_token.present? && partner_api_organization_id.present?
