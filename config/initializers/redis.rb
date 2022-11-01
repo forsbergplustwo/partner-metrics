835fd3e179301f800e64b9a5a7e061b2443b0fe2
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2022-11-01 13:59:35 +0100

    Redis fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 if ENV["REDIS_URL"]
-  $redis = Resque.redis = Redis.new(url: ENV["REDISCLOUD_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE})
+  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE})
 end
