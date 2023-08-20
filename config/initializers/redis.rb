07d99cfb23369f92377e9135f53a06dc69cc2435
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2023-08-21 00:00:48 +0200

    Remove  global

--- config/initializers/redis.rb
+++
@@ -1,3 +0,0 @@
-if ENV["REDIS_URL"]
-  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE})
-end
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,7 @@
-Resque.redis = $redis
+Resque.redis = if ENV["REDIS_URL"].blank?
+  Redis.new(host: "localhost", port: "6379")
+else
+  Redis.new(url: ENV["REDIS_URL"], ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE})
+end
 Resque.logger = Logger.new(STDOUT)
 Resque.logger.level = Logger::INFO
