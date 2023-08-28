663aefd5feeff3943e29dc3c36029c341e476504
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2023-08-28 14:44:27 +0200

    Add cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root to: "home#index"
+
+  resources :metrics, only: [:index]
+
+  namespace :metrics do
+    resources :recurring, :onetime, :affiliate, :summary, only: [:index]
+    resource :charts, only: [:show]
+  end
+
+  scope controller: :home do
+    post :import
+    post :import_status
+    get :app_store_analytics
+    get :reset_metrics
+    post :rename_app
+  end
 end
