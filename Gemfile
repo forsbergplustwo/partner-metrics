233f57a061fea357fe35742c8c4f92a52f01fef5
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2023-08-28 15:20:10 +0200

    Add chartkick

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "bootsnap", require: false
 gem "devise"
 gem "sidekiq"
 gem "graphql-client"
+gem "chartkick"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,10 @@
+Chartkick.options = {
+  height: "230px",
+  colors: ["#5912D5", "#007f5f", "#9C6ADE", "#F49342", "#454F5B", "#ED6347"],
+  library: {
+
+    vAxis: {textStyle: {color: "#616a75", fontSize: "14", paddingRight: "100", marginRight: "100"}, format: "short", gridlines: {color: "#F1F2F4"}},
+    hAxis: {textStyle: {color: "#616a75", fontSize: "14", paddingRight: "100", marginRight: "100"}},
+    chartArea: {width: "100%", left: "10%"}
+  }
+}
