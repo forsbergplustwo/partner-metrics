f6c69b879320a91ee26f2abd4a111e25b403f120
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2023-09-10 22:33:57 +0200

    Cleanup app renamer

--- app/controllers/rename_apps_controller.rb
+++ app/controllers/rename_apps_controller.rb
@@ -6,11 +6,12 @@ class RenameAppsController < ApplicationController
   end
 
   def create
-    from_name = rename_app_params[:from]
-    to_name = rename_app_params[:to]
-    if from_name.present? && to_name.present? && from_name != to_name
-      current_user.metrics.where(app_title: from_name).update_all(app_title: to_name)
-      current_user.payments.where(app_title: from_name).update_all(app_title: to_name)
+    app_renamer = AppRenamer.new(
+      user: current_user,
+      from: rename_app_params[:from],
+      to: rename_app_params[:to]
+    )
+    if app_renamer.rename!
       redirect_to rename_apps_path, notice: "App renamed successfully"
     else
       flash.now[:error] = "App rename failed"
