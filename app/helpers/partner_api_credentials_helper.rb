a42caa2182cd621d8ce22f84901e1e497b126516
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2023-09-13 16:23:32 +0200

    Cleanup partner api credentials + add banner instructions etc

--- app/helpers/partner_api_credentials_helper.rb
+++ app/helpers/partner_api_credentials_helper.rb
@@ -1,2 +1,9 @@
 module PartnerApiCredentialsHelper
+  def partner_api_credential_path_for(current_user)
+    if current_user.partner_api_credential&.persisted?
+      edit_partner_api_credential_path(current_user.partner_api_credential)
+    else
+      new_partner_api_credential_path
+    end
+  end
 end
